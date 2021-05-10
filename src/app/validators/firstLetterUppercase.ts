59d1d85b6618f980c88180dc0d6c06363ba402d8
Ben Gardiner <bennygards@gmail.com>
2021-05-10 16:19:15 +1000

    missing closing tag on mat-error

--- src/app/validators/firstLetterUppercase.ts
+++ src/app/validators/firstLetterUppercase.ts
@@ -1,24 +1,21 @@
+  
 import { AbstractControl, ValidatorFn } from "@angular/forms";
 
-//validator function is A function that receives a control and synchronously returns a map of validation errors if present, otherwise null.
-export function firstLetterUppercase(): ValidatorFn
-{
-    //abstract control is the base class for formControl, form group and form array so 
-    // i think this is creating an instance of a form control that will be able to cerate a custom error handler on the string thats passed in.
+export function firstLetterUppercase(): ValidatorFn {
     return (control: AbstractControl) => {
         const value = <string>control.value;
-        if(!value) return;
-        if(value.length === 0 ) return;
+        if (!value) return;
+        if (value.length === 0) return;
 
         const firstLetter = value[0];
         if (firstLetter !== firstLetter.toUpperCase()){
-            return{
+            return {
                 firstLetterUppercase :{
-                    //this is like the c# version of exception.ex => ex.message
-                    message: 'The fist letter must be uppercase'
+                    message: 'The first letter must be uppercase'
                 }
             }
         }
+
         return;
     }
 }
