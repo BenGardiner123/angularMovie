b14936f575bdac52a532d6e2edcd262a3d843b73
Ben Gardiner <bennygards@gmail.com>
2021-05-11 22:34:38 +1000

    marker wasnt showing up, fixed

--- src/app/utilities/map/map.component.ts
+++ src/app/utilities/map/map.component.ts
@@ -1,5 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-import { latLng, LeafletMouseEvent, marker, Marker, tileLayer } from 'leaflet';
+import { latLng, LeafletMouseEvent, marker, Marker, tileLayer, icon } from 'leaflet';
+import * as L from 'leaflet';
+
+//https://www.digitalocean.com/community/tutorials/angular-angular-and-leaflet-marker-service
+//even though i followed the directions form asyemetirux it didnt work ... this did though
+const iconRetinaUrl = 'assets/marker-icon-2x.png';
+const iconUrl = 'assets/marker-icon.png';
+const shadowUrl = 'assets/marker-shadow.png';
+const iconDefault = L.icon({
+  iconRetinaUrl,
+  iconUrl,
+  shadowUrl,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  tooltipAnchor: [16, -28],
+  shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = iconDefault;
+
 
 @Component({
   selector: 'app-map',
@@ -13,12 +32,15 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //set the basic options for when the map starts
   options = {
     layers: [
-      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: 'Angular Movies' })
+      tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { 
+        maxZoom: 18, 
+      attribution: 'Angular Movies' })
     ],
-    zoom: 14,
-    center: latLng(-26.531495547591952, 153.08187312024532)
+    zoom: 12,
+    center: latLng(-26.60873208945273,  153.05902632186192)
   };
 
   layers: Marker<any>[] = [];
@@ -27,7 +49,11 @@ export class MapComponent implements OnInit {
     const latitude = event.latlng.lat;
     const longitude = event.latlng.lng;
     console.log({latitude, longitude});
+    this.layers = [];
     this.layers.push(marker([latitude, longitude]));
-
+    //this.onSelectedLocation.emit({latitude, longitude});
   }
+
+ 
+
 }
