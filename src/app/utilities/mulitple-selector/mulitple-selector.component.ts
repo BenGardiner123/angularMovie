3a8b8511d40403ecb79d1308160a3adc2afe7092
Ben Gardiner <bennygards@gmail.com>
2021-05-12 10:54:51 +1000

    adding logic in the multple selector component

--- src/app/utilities/mulitple-selector/mulitple-selector.component.ts
+++ src/app/utilities/mulitple-selector/mulitple-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { multipleSelector, multipleSelectorModel } from './multiple-selector.model';
 
 @Component({
   selector: 'app-mulitple-selector',
@@ -9,17 +10,33 @@ export class MulitpleSelectorComponent implements OnInit {
 
   constructor() { }
 
-  
+  @Input()
+  SelectedItems: multipleSelector[] = [];
+
+  @Input()
+  NonSelectedItems: multipleSelector[] = [];
 
   ngOnInit(): void {
   }
 
-  selectAll(){
+  select(item: multipleSelectorModel, index: number){
+    this.SelectedItems.push(item);
+    this.NonSelectedItems.splice(index, 1);
+  }
 
+  deSelect(item: multipleSelectorModel, index: number){
+    this.NonSelectedItems.push(item);
+    this.SelectedItems.splice(index, 1);
+  }
+
+  selectAll(){
+    this.SelectedItems.push(...this.NonSelectedItems);
+    this.NonSelectedItems = [];
   }
 
   deSelectAll(){
-    
+    this.NonSelectedItems.push(...this.SelectedItems);
+    this.SelectedItems = [];
   }
 
 
--- src/app/utilities/mulitple-selector/multiple-selector.model.ts
+++ src/app/utilities/mulitple-selector/multiple-selector.model.ts
@@ -1,4 +1,5 @@
-export interface multipleSelector{
+export interface multipleSelectorModel{
     key: number;
-    
+    value: string;
+
 }
